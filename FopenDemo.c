9f4c2f9c0a7b80a76e0ecffda87fd083cc390a50
otadK <1647751528@qq.com>
2023-03-28 16:33:29 +0100

    FopenDemo

---
+++ FopenDemo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    FILE *fp;
+    char buffer[1024];
+
+    fp = fopen("FopenDemoFile.txt", "rw");
+
+    fread(buffer, 1024, 1024, fp);
+    printf("%s", buffer);
+    
+    // 可以用fgets替代fread
+    // while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    //     printf("%s", buffer);
+    // }
+    
+    fprintf(fp, "This is some text written to the file.\n");
+
+    fclose(fp);
+
+    return 0;
+}
