18c884b36bc1e59d2cf6415d92f7d9422df7f5ce
otadK <1647751528@qq.com>
2023-03-29 15:32:44 +0100

    add common

---
+++ common/head.h
@@ -0,0 +1,18 @@
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "color.h"
+#include "common.h"
+
+// gcc xxx.c -D _D to start Debug mode
+#ifdef _D
+#define DBG(fmt, args...) printf(fmt, ##args)
+#else
+#define DBG(fmt, args...)
+#endif
+
+#endif
